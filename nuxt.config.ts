4b266d8bad3be2efdd022be8d37e44b7f3e86dd8
Guillermo Cava <gcavanunez@gmail.com>
2023-09-04 18:15:07 -0500

    lets go again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	modules: [
-		"@sidebase/nuxt-auth",
+		// "@sidebase/nuxt-auth",
 		"@nuxt/devtools",
 		"@nuxtjs/color-mode",
 		"@vueuse/nuxt",
